bfe075e46b63b27df0d7051f0ade6fbc36e3a7ff
ben strawbridge <ben@benstrawbridge.com>
2024-01-31 12:17:43 -0800

    Update backgroundChanger.js
    add some more images for testing

--- assets/js/backgroundChanger.js
+++ assets/js/backgroundChanger.js
@@ -7,6 +7,9 @@ const backgroundImageList = [
   // add links to your shared google photos image directly here
   'https://lh3.googleusercontent.com/pw/ABLVV84PckiqmIJNE5iB3BI_vV-grDSeDQfjyyLolAE1_t_No1Z_IlzgI9UJ5rvabL5U-gnT_v7_S07qkzF-ucjzEJT5kLFwtUaLwfebH-2R4GiUDEIukIfOHaEVi_JECfmXOyDDAsb3zwNfaZN78b2lXbwxgg=w613-h1088-s-no-gm?authuser=0',
   'https://lh3.googleusercontent.com/pw/ABLVV854B1XWYwZtzSIxhizEmGnrW1jgdyI0P9gQ942oI715M_4mGXWUIniRb5p5xedTx9WS4_nGIB_IOdK9ypRNDDPStmqwpwMA_RdC6NwtolzRe1uN0d6_NIISimDXWuiuM91pzNh4RMtpyUkybPcg3hWHxw=w613-h1088-s-no-gm?authuser=0',
+  'https://lh3.googleusercontent.com/pw/ABLVV86h_0kvKZ-eRI1WJhUOpIRk0elQ0rypGEWcQJnHX5QgLBYY3KqKwCrVn7hVIhKZoCymxjg_P8tHbMaPqg2iY-EeODHvcG4hv1VTVf1dyPCoEhm9qP1as1EEgK_wVJ6oq4cuO548n4i1CoQzO3FncLdm6Q=w1155-h650-s-no-gm?authuser=0',
+  'https://lh3.googleusercontent.com/pw/ABLVV872UJW6LsZ3ecRRu0Q97APztiiW2gjxooi-fJSMaosj_KCMLrWeoTnkZwXJGfEd52lZHSY78vZUGlqdZ3hVAN0WQ_Ogw2fW5e2z6f_yIs9ZXZPB3XroeCNaPfFDxpieIt9Y4gZAaF5yeU175SY-nK4P8g=w612-h1088-s-no-gm?authuser=0',
+  'https://lh3.googleusercontent.com/pw/ABLVV86tz5bkwmjiMkPBtuWfrpuoIjW_h3ZLbiQKIb4XhWe0JTTKxrPpNa9diFj-PB0UYixQl7bTACVoj-pdHGQI68LZCgM7lBbHVlCB8afJ_k_F0hotPQDRIrMdlS_ZY6t7HDinj_qivFvabsLVubPmT5wE_g=w1155-h650-s-no-gm?authuser=0',
 ];
 
 function getRandomBackgroundImage() {
