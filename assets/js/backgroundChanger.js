40558eb143a74008a59f53c0a5ac44e2ceb6dc87
ben strawbridge <ben@benstrawbridge.com>
2024-01-31 12:26:30 -0800

    Update backgroundChanger.js

--- assets/js/backgroundChanger.js
+++ assets/js/backgroundChanger.js
@@ -10,6 +10,8 @@ const backgroundImageList = [
   'https://lh3.googleusercontent.com/pw/ABLVV86h_0kvKZ-eRI1WJhUOpIRk0elQ0rypGEWcQJnHX5QgLBYY3KqKwCrVn7hVIhKZoCymxjg_P8tHbMaPqg2iY-EeODHvcG4hv1VTVf1dyPCoEhm9qP1as1EEgK_wVJ6oq4cuO548n4i1CoQzO3FncLdm6Q=w1155-h650-s-no-gm?authuser=0',
   'https://lh3.googleusercontent.com/pw/ABLVV872UJW6LsZ3ecRRu0Q97APztiiW2gjxooi-fJSMaosj_KCMLrWeoTnkZwXJGfEd52lZHSY78vZUGlqdZ3hVAN0WQ_Ogw2fW5e2z6f_yIs9ZXZPB3XroeCNaPfFDxpieIt9Y4gZAaF5yeU175SY-nK4P8g=w612-h1088-s-no-gm?authuser=0',
   'https://lh3.googleusercontent.com/pw/ABLVV86tz5bkwmjiMkPBtuWfrpuoIjW_h3ZLbiQKIb4XhWe0JTTKxrPpNa9diFj-PB0UYixQl7bTACVoj-pdHGQI68LZCgM7lBbHVlCB8afJ_k_F0hotPQDRIrMdlS_ZY6t7HDinj_qivFvabsLVubPmT5wE_g=w1155-h650-s-no-gm?authuser=0',
+  'https://lh3.googleusercontent.com/pw/ABLVV875N7UIlnDhom-ggcxRAYKB4zGp4tdJlfg92qBNhdq2Ne761IbqOGpIONrThF4OLWnoim_bklMle8y4YRQ_m8ir6Lpemve2p0FRd14wAL2zjHlSZY_yt5bnizUyWuS5lG3vPuCMjZRUxW1-w2-_IPZ8Hw=w1155-h650-s-no-gm?authuser=0',
+  'https://lh3.googleusercontent.com/pw/ABLVV86qJgZ2EdKX62hCsb4yXbCNaUmgWac9sMfvS0hnfsqsswhWCwOEAe8oZId48tod8Cm9C_X0wqPL1MHkrmQDOpzQGrQ6zzpId0IVGTAWyiRJEsGtwx_GHueF3yLM8Xg7LzfyF40EzWI46ydmtG6wnajiVA=w1155-h650-s-no-gm?authuser=0',
 ];
 
 function getRandomBackgroundImage() {
