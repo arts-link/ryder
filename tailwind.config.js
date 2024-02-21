94c53c85f27528aba6cdb8d81032cdb268e27ffc
ben strawbridge <ben@benstrawbridge.com>
2024-02-21 07:04:42 -0800

    need to keep exampleSite honest!
    - fix pages build failure for example site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html"],
+  content: [
+    "./hugo_stats.json",
+    "content/**/*.md", 
+    "layouts/**/*.html"
+    "./themes/ryder/layouts/**/*.html",
+  ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
         'hidden-home': " url('/images/hidden-home-cover.webp')",
-      },},
+        "header-sunset": " url('/images/header-bg/sunset-playa-1.jpg')",
+        "header-sunset-italy": " url('/images/header-bg/sunset-italy.jpg')",
+        // "header-sunset-mb": " url('/images/header-bg/sunset-mission-bay.jpg')",
+        "header-sunset-mb": " url('/images/header-bg/sunset-mission-bay_hu6f04b8530673b6e2cc009e9b6d51ea4d_1824404_1024x768_resize_q100_h2_box.webp')",
+      },
+      fontFamily: {
+        titillium: ["Titillium Web", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        'xs': '475px',
+        ...defaultTheme.screens,
+        '3xl': '1600px',
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
