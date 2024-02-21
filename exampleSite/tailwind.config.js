71f6702d4e9e5ab3db35f711c89c835b62dc0e80
ben strawbridge <ben@benstrawbridge.com>
2024-02-21 07:16:00 -0800

    grrrrr

--- exampleSite/tailwind.config.js
+++ exampleSite/tailwind.config.js
@@ -1,17 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
+  // content: ['./hugo_stats.json'],
+
   content: [
-    "./hugo_stats.json", 
-    "./config/**/*.toml",
+    "'./hugo_stats.json'",
     "./layouts/**/*.html",
-    "./../layouts/**/*.html", 
-    "./themes/ryder/layouts/**/*.html", 
+    "./config/**/*.toml",
+    "./content/**/*.md",
+    "./themes/benstraw/layouts/**/*.html",
+    "./themes/ryder/layouts/**/*.html",
+    "./themes/ryder-dev/layouts/**/*.html",
   ],
   darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        'hidden-home': " url('../images/hidden-home-cover.webp')",
+        "hidden-home": " url('/images/hidden-home-cover.webp')",
+        "header-sunset": " url('/images/header-bg/sunset-playa-1.jpg')",
+        "header-sunset-italy": " url('/images/header-bg/sunset-italy.jpg')",
+        // "header-sunset-mb": " url('/images/header-bg/sunset-mission-bay.jpg')",
+        "header-sunset-mb": " url('/images/header-bg/sunset-mission-bay_hu6f04b8530673b6e2cc009e9b6d51ea4d_1824404_1024x768_resize_q100_h2_box.webp')",
+      },
+      fontFamily: {
+        titillium: ["Titillium Web", ...defaultTheme.fontFamily.sans],
       },
       screens: {
         'xs': '475px',
@@ -21,5 +34,4 @@ module.exports = {
     },
   },
   plugins: [require("@tailwindcss/typography")],
-}
-
+};
